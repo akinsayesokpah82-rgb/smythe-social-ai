7ac5f83fb1d39c50897fa7986fa112109e73755c
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-04 23:47:44 +0000

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to Smythe Social AI</h1>
+      <p>Your app is deployed successfully on Netlify 🎉</p>
+    </main>
+  );
+}
